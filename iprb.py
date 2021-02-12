0509fbba7df0ccd76f7934411997e6343f2519d6
Noah Syrkis <noah@syrkis.com>
2021-02-12 10:04:05 +0100

    add iprb

--- iprb.py
+++ iprb.py
@@ -1,11 +1,27 @@
-"""
-k individuals have two of G
-m individuals have one G and one not G
-n individuals have no G
-"""
-
 from sys import stdin
+
 k, m, n = map(int, stdin.read().strip().split())
 N = sum([k, m, n])
+l = [k, m, n]
+tmp = 0
 
 
+for i in range(len(l)):
+    p_1 = l[i] / N 
+    for j in range(len(l)):
+        if i == j:
+            f = l[j] - 1 
+        else:
+            f = l[j]
+        p_2 = f / (N - 1)
+        if 0 in [i, j]:
+            f = 1
+        elif set([i, j]) == set([1, 2]):
+            f = .5
+        elif set([i, j]) == set([1]):
+            f = .75
+        else:
+            f = 0
+        tmp += p_1 * p_2 * f
+
+print(tmp)
