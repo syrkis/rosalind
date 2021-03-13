9014c54583fa304521aca3a5c9d474234071d325
Noah Syrkis <noah@syrkis.com>
2021-03-13 18:00:07 +0100

    read the instructions

--- grph.py
+++ grph.py
@@ -1,24 +1,8 @@
 from sys import stdin
-data = [[entry.strip()[:13], entry.strip()[14:].replace('\n', '')] for entry in stdin.read().split('>')[1:]]
+data = [[line.split('\n')[0], "".join(line.split('\n')[1:])] for line in stdin.read().split('>')]
+D = {entry[0]: entry[1] for entry in data}
 
-
-def suffix(s, t, k):
-    if s[:k] in t[1:]:
-        return True
-
-A = set()
-
-for i in range(len(data)):
-    for j in range(len(data)):
-        if i != j and suffix(data[i][1], data[j][1], 3):
-            if int(data[i][0][-4:]) < int(data[j][0][-4:]):
-                mi = data[i][0]
-                ma = data[j][0]
-            else:
-                mi = data[j][0]
-                ma = data[i][0]
-            A.add(f"{mi} {ma}")
-
-                
-for l in list(A):
-    print(l)
+for ki, vi in D.items():
+    for kj, vj in list(D.items()):
+        if ki != kj and vi[:3] == vj[-3:]:
+            print(kj, ki)
