a715ee5cc0320f41cfea74ea2ae1ff835ec4b420
Noah Syrkis <noah@syrkis.com>
2021-03-20 09:57:45 +0100

    O(n ** 3) solution

--- lcsm.py
+++ lcsm.py
@@ -1,9 +1,26 @@
 from sys import stdin
 data = ["".join(line.split()[1:]) for line in stdin.read().split('>')[1:]]
 
-m = min([len(line) for line in data]) 
+def ngram(seq, n):
+    ngrams = []
+    for i in range(len(seq) - (n - 1)):
+        ngrams.append(seq[i:i+n])
+    return ngrams
 
-def lcs(S, m):
-    for s in S:
-        for i in range(len(s)-m):
-            if 
+def inall(seqs, gram, n):
+    for seq in seqs:
+        ngrams = ngram(seq, n)
+        if gram not in ngrams:
+            return False
+    return True
+
+def runner():
+    for i in range(min([len(seq) for seq in data])):
+        n = min([len(seq) for seq in data]) - i
+        for seq in data:
+            ngrams = ngram(seq, n)
+            for gram in ngrams:
+                if inall(data, gram, n):
+                    return gram
+
+print(runner())
