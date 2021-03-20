5d4bef0463dfe76101a2ca8bf9c543a8e43c8937
Noah Syrkis <noah@syrkis.com>
2021-03-20 12:04:35 +0100

    lcsm fucking fast

--- lcsm.py
+++ lcsm.py
@@ -1,26 +1,26 @@
 from sys import stdin
-data = ["".join(line.split()[1:]) for line in stdin.read().split('>')[1:]]
+
+
+def lcsm(data, S=set()):
+    if not data:
+        return sorted(list(S), key= lambda x: len(x))[-1]
+    n = 1000 if len(S) == 0 else max([len(e) for e in list(S)])  
+    T = ngrams(S, data[0], n) 
+    return lcsm(data[1:], T)
+  
+def ngrams(S, seq, n):
+    T = set()
+    for i in range(1, n):
+        for e in ngram(seq, i):
+            T.add(e)   
+    return S.intersection(T) if len(S) != 0 else T
 
 def ngram(seq, n):
-    ngrams = []
+    out = []
     for i in range(len(seq) - (n - 1)):
-        ngrams.append(seq[i:i+n])
-    return ngrams
+        out.append(seq[i:i+n+1])
+    return out
 
-def inall(seqs, gram, n):
-    for seq in seqs:
-        ngrams = ngram(seq, n)
-        if gram not in ngrams:
-            return False
-    return True
-
-def runner():
-    for i in range(min([len(seq) for seq in data])):
-        n = min([len(seq) for seq in data]) - i
-        for seq in data:
-            ngrams = ngram(seq, n)
-            for gram in ngrams:
-                if inall(data, gram, n):
-                    return gram
-
-print(runner())
+data = ["".join(line.split()[1:]) for line in stdin.read().split('>')[1:]]
+out = lcsm(data)
+print(out)
