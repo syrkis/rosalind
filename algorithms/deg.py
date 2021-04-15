951ffcfec95fbf9558e6bc15a9d02bd7f099ab9e
Noah Syrkis <noah@syrkis.com>
2021-04-15 23:00:47 +0200

    begin pper

--- algorithms/deg.py
+++ algorithms/deg.py
@@ -1,14 +1,16 @@
 from collections import defaultdict
 
 with open('../data/rosalind_deg.txt', 'r') as f:
-    E = list(map(int, f.read().strip().split()))
-    N = set([E[i] for i in range(0, len(E), 2)])
-    print(N)
+    data = [list(map(int, edge.split())) for edge in f.read().strip().split('\n')]
+
+n = set([edge[0] for edge in data])
 
 D = defaultdict(int)
 
-for n in E:
-      
-    D[n] += 1
+for e in data:
+    if e[0] in n and e[1] in n:
+        D[e[0]] += 1
+        D[e[1]] += 1
 
-print(sorted(D.items(), key=lambda x: x[0]))
+out = sorted(D.items(), key=lambda x: x[0])
+print(out)
---
+++ stronghold/pper.py
@@ -0,0 +1,9 @@
+def fact(n):
+    if n == 2:
+        return 2
+    return n * fact(n-1)
+
+def pper(n, k):
+    return 
+
+print(pper(21, 7))
